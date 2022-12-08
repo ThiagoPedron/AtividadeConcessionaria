9aa6a1899985d72496df259e5e8bc70ea9f6f3ca
ThiagoPedron <thiagopedron70@gmail.com>
2022-12-08 10:36:17 -0300

    teste

--- UI/front-concessionaria/src/app/patio/patio.component.ts
+++ UI/front-concessionaria/src/app/patio/patio.component.ts
@@ -9,26 +9,40 @@ import { Area } from '../interfaces/areas';
   styleUrls: ['./patio.component.css']
 })
 export class PatioComponent implements OnInit {
-
+  teste : Array<number> = [1,2,3,4,5,5,6,6,7]
   areaId: number = 0
   lista_areas : Array<Area> = []
   lista_locacao: Array<Alocacao> = []
   lista_automoveis: Array<Automoveis> = []
   automovel_venda: Automoveis | any
+
+  
   constructor() {
-   this.getAllAreas();
+   this.getAlocacao();
+   this.lista_areas = [
+    {'id':1,'quantidade':0},
+    {'id':2,'quantidade':0},
+    {'id':3,'quantidade':0},
+    {'id':4,'quantidade':0},
+    {'id':5,'quantidade':0},
+    {'id':6,'quantidade':0},
+    {'id':7,'quantidade':0},
+    {'id':8,'quantidade':0},
+    {'id':9,'quantidade':0},
+    {'id':10,'quantidade':0},
+    {'id':11,'quantidade':0},
+   ]
   }
 
   ngOnInit(): void {
 
   }
 
-
   chama_venda(id: number) {
     this.areaId = id
 
   }
-  getAllAreas() {
+  getAlocacao() {
     var config = {
       method: 'get',
       url: 'http://localhost:5224/Alocacao/alocacao/getAll',
@@ -37,10 +51,17 @@ export class PatioComponent implements OnInit {
     let instance = this;
     axios(config).then(function (response:any) {
       console.log(response.data);
-      instance.lista_locacao = response.data;
-      instance.lista_locacao.forEach(locacao => {
-        
-      });
+      instance.lista_locacao = response.data
+      instance.verificaAutomovelAlocacao()
+      console.log(instance.lista_areas)
+    });
+  }
+  verificaAutomovelAlocacao(){
+    this.lista_locacao.forEach(element => {
+       this.lista_areas.forEach(area => {
+          if(area.id == element.area)
+            area.quantidade +=1
+       });
     });
   }
 }
